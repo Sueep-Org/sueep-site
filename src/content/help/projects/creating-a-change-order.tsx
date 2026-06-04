aecea44ad7050b3f4d6dcfbf29b0d1702570adca
EmmaMazzoni <emazzonix7@gmail.com>
2026-06-04 11:45:06 -0400

    Fix deployment error. Issue with quotations.

--- src/content/help/projects/creating-a-change-order.tsx
+++ src/content/help/projects/creating-a-change-order.tsx
@@ -1,4 +1,4 @@
-import { H2, P, Steps, Step, Callout, Img, SkipTo } from "@/app/erp/components/help/HelpComponents";
+import { H2, P, Steps, Step, Img, SkipTo } from "@/app/erp/components/help/HelpComponents";
 
 export function CreatingAChangeOrder() {
   return (
@@ -14,7 +14,7 @@ export function CreatingAChangeOrder() {
             <Step n={2} title="Methods of Creating a Change Order">
                 There are 2 ways we can create a change order:
                 <ol className="mt-3 mb-3 space-y-1.5 pl-5 text-sm text-gray-700 list-decimal">
-                    <li>Via the <SkipTo href="#NewProject">"New Project"</SkipTo> button</li>
+                    <li>Via the <SkipTo href="#NewProject">&ldquo;New Project&rdquo;</SkipTo> button</li>
                     <li>Via <SkipTo href="#ProjectDetails">Project Details</SkipTo></li>
                 </ol>
                 We will go over both. They do the same thing where you chose to create it is based on preference.
@@ -22,9 +22,9 @@ export function CreatingAChangeOrder() {
 
             <div id= "NewProject">
             <Step n={3} title="Via the New Project Button">
-                Click on the New Project button from the projects page
+                Click on the New Project button from the projects page.
                 <Img src="/help/change_order/new_project_1.png" alt="New Project Button" />
-                Under "Segment" select change order
+                Under &ldquo;Segment&rdquo; select change order
                 <Img src="/help/change_order/new_project_2.png" alt="New Projects form" />
                 Fill out your change order information
                 <Img src="/help/change_order/new_project_3.png" alt="Change Order form" />
@@ -37,7 +37,7 @@ export function CreatingAChangeOrder() {
             <Step n={4} title="Via the Project Details page">
                 Navigate to the Projects tab and click on your desired project.
                 <Img src="/help/change_order/project_details_1.png" alt="Projects Tab" />
-                Navigate to the "Change Orders" tab
+                Navigate to the &ldquo;Change Orders&rdquo; tab
                 <Img src="/help/change_order/project_details_2.png" alt="Project details page" />
 
             </Step>
